73cad545bee160f53aa7678c1f012193db95b957
Stanislav Vitko <vist.rcm@gmail.com>
2014-09-17 18:47:49 +0400

    initial implementation of GoGridClient

---
+++ src/main/scala/info/vitko/gogrid/GoGridClient.scala
@@ -0,0 +1,45 @@
+package info.vitko.gogrid
+
+import java.security.MessageDigest
+
+import dispatch.Defaults._
+import dispatch._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vist
+ * Date: 17/09/14
+ * Time: 17:03
+ */
+class GoGridClient(key: String, secret: String,
+                   apiUrl: String = "https://api.gogrid.com/api",
+                   version: String = "1.9") {
+
+  def listServers() = {
+    this.apiRequest("/grid/server/list")
+  }
+
+  private def apiRequest(path: String, format: String = "json") = {
+    val requestUrl = s"${this.apiUrl}/grid/server/list"
+    val svc = url(requestUrl) <<? Map("api_key" -> key) <<? Map("sig" -> getSig()) <<?
+      Map("v" -> version) <<? Map("format" -> format)
+    val resp = Http(svc OK as.String)
+    resp()
+  }
+
+  private def getSig() = {
+    val timestamp = System.currentTimeMillis / 1000
+    val string = key + secret + timestamp
+    val digest = MessageDigest.getInstance("MD5")
+    digest.digest(string.getBytes).map("%02x".format(_)).mkString
+  }
+
+}
+
+object GoGridClient extends App {
+  val secret = "xxxxx"
+  val key = "xxxxx"
+
+  val client = new GoGridClient(key, secret)
+  println(client.listServers())
+}
