8929b6715f905eda65e564f1fa32dce1756768bb
Stanislav Vitko <vist.rcm@gmail.com>
2014-09-19 19:25:25 +0400

    GoGridJob description added

---
+++ src/main/scala/info/vitko/gogrid/GoGridJob.scala
@@ -0,0 +1,94 @@
+package info.vitko.gogrid
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vist
+ * Date: 19/09/14
+ * Time: 16:01
+ */
+
+import scalaz._, Scalaz._
+import argonaut._, Argonaut._
+
+case class JobCommand(description: String, id: Int, name: String, obj: String)
+
+object JobCommand {
+  // Define codecs easily from case classes
+  implicit def JobCommandCodecJson: CodecJson[JobCommand] =
+    casecodec4(JobCommand.apply, JobCommand.unapply)("description", "id", "name", "object")
+}
+
+case class JobCurrentstate(description: String, id: Int, name: String, obj: String)
+
+object JobCurrentstate {
+  implicit def JobCurrentstateCodecJson: CodecJson[JobCurrentstate] =
+    casecodec4(JobCurrentstate.apply, JobCurrentstate.unapply)("description", "id", "name", "object")
+}
+
+case class JobDatacenter(description: String, id: Int, name: String, obj: String)
+
+object JobDatacenter {
+  implicit def JobDatacenterCodecJson: CodecJson[JobDatacenter] =
+    casecodec4(JobDatacenter.apply, JobDatacenter.unapply)("description", "id", "name", "object")
+}
+
+case class JobDetail(description: Option[String], image: String, ip: String, name: String, detailType: String)
+
+object JobDetail {
+  implicit def JobDetailCodecJson: CodecJson[JobDetail] =
+    casecodec5(JobDetail.apply, JobDetail.unapply)("description", "image", "ip", "name", "type")
+}
+
+case class JobHistoryEntryState(description: String, id: Int, name: String, obj: String)
+
+object JobHistoryEntryState {
+  implicit def JobHistoryEntryStateCodecJson: CodecJson[JobHistoryEntryState] =
+    casecodec4(JobHistoryEntryState.apply, JobHistoryEntryState.unapply)("description", "id", "name", "object")
+}
+
+case class JobHistoryEntry(id: Int, state: JobHistoryEntryState, updatedon: Option[Int])
+
+object JobHistoryEntry {
+  implicit def JobHistoryEntryCodecJson: CodecJson[JobHistoryEntry] =
+    casecodec3(JobHistoryEntry.apply, JobHistoryEntry.unapply)("id", "state", "updatedon")
+}
+
+case class JobObjecttype(description: Option[String], id: Int, name: String, obj: String)
+
+object JobObjecttype {
+  implicit def JobObjecttypeCodecJson: CodecJson[JobObjecttype] =
+    casecodec4(JobObjecttype.apply, JobObjecttype.unapply)("description", "id", "name", "object")
+}
+
+case class GoGridJob(attempts: Int,
+                     command: JobCommand,
+                     createdon: Int,
+                     currentstate: JobCurrentstate,
+                     datacenter: JobDatacenter,
+                     detail: JobDetail,
+                     history: List[JobHistoryEntry],
+                     id: Int,
+                     lastupdatedon: Option[Int],
+                     obj: String,
+                     objecttype: JobObjecttype,
+                     owner: String)
+
+object GoGridJob {
+  implicit def GoGridJobCodecJson: CodecJson[GoGridJob] =
+    casecodec12(GoGridJob.apply, GoGridJob.unapply)("attempts", "command", "createdon", "currentstate",
+      "datacenter", "detail", "history", "id", "lastupdatedon", "object", "objecttype", "owner")
+}
+
+case class JobsResponseSummary(numpages: Int, start: Int, total: Int, returned: Int)
+
+object JobsResponseSummary {
+  implicit def JobsResponseCodecJson: CodecJson[JobsResponseSummary] =
+  casecodec4(JobsResponseSummary.apply, JobsResponseSummary.unapply)("numpages", "start", "total", "returned")
+}
+
+case class JobsResponse(jobs: List[GoGridJob], method: String, status: String, summary: JobsResponseSummary)
+
+object JobsResponse {
+  implicit def JobsResponseCodecJson: CodecJson[JobsResponse] =
+    casecodec4(JobsResponse.apply, JobsResponse.unapply)("list", "method", "status", "summary")
+}
