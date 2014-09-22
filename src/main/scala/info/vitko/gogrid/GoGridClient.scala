c53b0460faa4e9830b4aabdf3b069639f462817a
Stanislav Vitko <vist.rcm@gmail.com>
2014-09-22 21:42:29 +0400

    simplify code a little bit

--- src/main/scala/info/vitko/gogrid/GoGridClient.scala
+++ src/main/scala/info/vitko/gogrid/GoGridClient.scala
@@ -14,11 +14,10 @@ import argonaut._, Argonaut._
  * Time: 17:03
  */
 class GoGridClient(key: String, secret: String,
-                   apiUrl: String = "https://api.gogrid.com/api",
-                   version: String = "1.9") {
+                   apiUrl: String = GoGridConfig.apiUrl,
+                   version: String = GoGridConfig.version) {
 
-  def this() = this(GoGridConfig.AccessConfig.key, GoGridConfig.AccessConfig.secret,
-                    GoGridConfig.apiUrl, GoGridConfig.version)
+  def this() = this(GoGridConfig.AccessConfig.key, GoGridConfig.AccessConfig.secret)
 
   def listServers() = apiRequest("grid/server/list")
 
